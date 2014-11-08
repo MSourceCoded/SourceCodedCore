2683c368f4c14beca62d05501ca8fcc0eb80f5bf
MSourceCoded <sourcecodedcreations@gmail.com>
2014-11-08 16:03:17 +0800

    Fixes not being able to take screenshots in GUIs

--- src/main/java/sourcecoded/core/gameutility/screenshot/ScreenshotTickHandler.java
+++ src/main/java/sourcecoded/core/gameutility/screenshot/ScreenshotTickHandler.java
@@ -2,8 +2,10 @@ package sourcecoded.core.gameutility.screenshot;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.InputEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.IChatComponent;
+import org.lwjgl.input.Keyboard;
 import sourcecoded.core.SourceCodedCore;
 import sourcecoded.core.configuration.SCConfigManager;
 
@@ -11,13 +13,29 @@ import javax.sql.rowset.serial.SerialRef;
 
 public class ScreenshotTickHandler {
 
+    public boolean isTakingScreenshotCurrently = false;         //Only run once
+
+//    @SubscribeEvent                   Does not work in GUIs >.<
+//    public void keyEvent(InputEvent.KeyInputEvent event) {
+//        Minecraft mc = Minecraft.getMinecraft();
+//        if (SourceCodedCore.keyScreenshot.isPressed()) {
+//            IChatComponent data = SCScreenshotHandler.saveScreenshot(mc.mcDataDir, mc.displayWidth, mc.displayHeight, mc.getFramebuffer());
+//            if (SCConfigManager.getBoolean(SCConfigManager.Properties.SCREENSHOT_MESSAGE)) mc.ingameGUI.getChatGUI().printChatMessage(data);
+//        }
+//    }
+
     @SubscribeEvent
-    public void keyEvent(InputEvent.KeyInputEvent event) {
-        Minecraft mc = Minecraft.getMinecraft();
-        if (SourceCodedCore.keyScreenshot.isPressed()) {
-            IChatComponent data = SCScreenshotHandler.saveScreenshot(mc.mcDataDir, mc.displayWidth, mc.displayHeight, mc.getFramebuffer());
-            if (SCConfigManager.getBoolean(SCConfigManager.Properties.SCREENSHOT_MESSAGE)) mc.ingameGUI.getChatGUI().printChatMessage(data);
-        }
+    public void tick(TickEvent.ClientTickEvent event) {
+        if (Keyboard.isKeyDown(SourceCodedCore.keyScreenshot.getKeyCode())) {
+            if (!isTakingScreenshotCurrently) {
+                Minecraft mc = Minecraft.getMinecraft();
+                IChatComponent data = SCScreenshotHandler.saveScreenshot(mc.mcDataDir, mc.displayWidth, mc.displayHeight, mc.getFramebuffer());
+                if (mc.thePlayer != null && SCConfigManager.getBoolean(SCConfigManager.Properties.SCREENSHOT_MESSAGE))
+                    mc.ingameGUI.getChatGUI().printChatMessage(data);
+            }
+
+            isTakingScreenshotCurrently = true;
+        } else isTakingScreenshotCurrently = false;
     }
 
 }
