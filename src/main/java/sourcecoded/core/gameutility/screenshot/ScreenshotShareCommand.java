65fd100442525bdd4d1690470f6e1de52e9ca720
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2014-11-19 04:51:32 +0200

    Fix screenshot uploading, add a clickable link

--- src/main/java/sourcecoded/core/gameutility/screenshot/ScreenshotShareCommand.java
+++ src/main/java/sourcecoded/core/gameutility/screenshot/ScreenshotShareCommand.java
@@ -23,6 +23,7 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.IChatComponent;
 
 import org.apache.commons.codec.binary.Base64;
 
@@ -68,7 +69,12 @@ public class ScreenshotShareCommand extends CommandBase {
                                 String url = data.get("link").getAsString();
 
                                 if (success) {
-                                    Minecraft.getMinecraft().thePlayer.addChatComponentMessage(new ChatComponentText(EnumChatFormatting.GOLD + "Imgur upload successful! Link copied to clipboard! " + EnumChatFormatting.AQUA + EnumChatFormatting.ITALIC + "[" + url + "]"));
+                                    String str = "[{\"text\":\"imgur upload successful, link copied to clipboard! \",\"color\":\"white\"}"
+                                            + ",{\"text\":\"" + url + "\",\"color\":\"aqua\",\"hoverEvent\":{\"action\":\"show_text\",\"value\":"
+                                            + "{\"text\":\"Click to open the link\",\"color\":\"yellow\"}}"
+                                            + ",\"clickEvent\":{\"action\":\"open_url\",\"value\":\"" + url + "\"}}]";
+                                    IChatComponent chat = IChatComponent.Serializer.func_150699_a(str);
+                                    Minecraft.getMinecraft().thePlayer.addChatMessage(chat);
                                     Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
                                     clipboard.setContents(new StringSelection(url), null);
                                 } else {
@@ -81,8 +87,10 @@ public class ScreenshotShareCommand extends CommandBase {
                     }
                 }).start();
             }
+        } else if (args.length == 0) {
+            throw new WrongUsageException(getCommandUsage(sender));
         } else {
-            throw new WrongUsageException("No screenshot was taken this session!", "");
+            throw new WrongUsageException("You have to take a screenshot first!");
         }
     }
 
@@ -98,8 +106,7 @@ public class ScreenshotShareCommand extends CommandBase {
         conn.setRequestMethod("POST");
         conn.setRequestProperty("Authorization", "Client-ID " + clientID);
         conn.setRequestMethod("POST");
-        conn.setRequestProperty("Content-Type",
-                "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
         conn.connect();
         StringBuilder stb = new StringBuilder();
@@ -107,8 +114,7 @@ public class ScreenshotShareCommand extends CommandBase {
         wr.write(data);
         wr.flush();
 
-        BufferedReader rd = new BufferedReader(
-                new InputStreamReader(conn.getInputStream()));
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
         String line;
         while ((line = rd.readLine()) != null) {
             stb.append(line).append("\n");
@@ -125,10 +131,9 @@ public class ScreenshotShareCommand extends CommandBase {
         ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
         ImageIO.write(image, "png", byteArray);
         byte[] byteImage = byteArray.toByteArray();
-        String dataImage = Base64.encodeBase64URLSafeString(byteImage);
+        String dataImage = Base64.encodeBase64String(byteImage);
 
-        return URLEncoder.encode("image", "UTF-8") + "="
-                + URLEncoder.encode(dataImage, "UTF-8");
+        return URLEncoder.encode("image", "UTF-8") + "=" + URLEncoder.encode(dataImage, "UTF-8");
     }
 
     @SuppressWarnings("rawtypes")
